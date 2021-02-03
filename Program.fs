3f141ed7a5863216d920b2fc65ae9b42824b3871
Travis <tssund93@gmail.com>
2021-02-03 17:44:32 -0600

    implement add, info, and remove commands

--- Program.fs
+++ Program.fs
@@ -1,5 +1,7 @@
-open System
 open Argu
+open System.Text.Json
+open System
+open System.IO
 
 type BackupArgs =
     | [<MainCommand>] Games of games: string list
@@ -30,6 +32,7 @@ type AddArgs =
 
 type InfoArgs =
     | [<MainCommand>] Games of games: string list
+    | [<AltCommandLine("-b")>] Brief
 
     interface IArgParserTemplate with
         member this.Usage =
@@ -37,14 +40,7 @@ type InfoArgs =
             | Games _ ->
                 "List of games to display info for. If not \
                 provided, will display info for all games"
-
-type ListArgs =
-    | Verbose
-
-    interface IArgParserTemplate with
-        member this.Usage =
-            match this with
-            | Verbose -> "Show verbose output"
+            | Brief -> "Output only game names"
 
 type RemoveArgs =
     | [<MainCommand; Mandatory>] Games of games: string list
@@ -90,7 +86,6 @@ type ConfigArgs =
 type SbuArgs =
     | [<CliPrefix(CliPrefix.None)>] Backup of ParseResults<BackupArgs>
     | [<CliPrefix(CliPrefix.None)>] Add of ParseResults<AddArgs>
-    | [<CliPrefix(CliPrefix.None)>] List of ParseResults<ListArgs>
     | [<CliPrefix(CliPrefix.None)>] Info of ParseResults<InfoArgs>
     | [<CliPrefix(CliPrefix.None)>] Remove of ParseResults<RemoveArgs>
     | [<CliPrefix(CliPrefix.None)>] Edit of ParseResults<EditArgs>
@@ -103,7 +98,6 @@ type SbuArgs =
             match this with
             | Backup _ -> "Backup your game saves"
             | Add _ -> "Add games to backup"
-            | List _ -> "List games that can be backed up"
             | Info _ -> "List info for games"
             | Remove _ -> "Remove games from backup"
             | Edit _ -> "Edit game info"
@@ -111,20 +105,116 @@ type SbuArgs =
             | Config_Path _ -> "Path to configuration file"
             | Version -> "Print version information"
 
-let backup (games: string list option) (loop: bool) (verbose: bool) = printfn $"{games} {loop} {verbose}"
-let add (game: string) (path: string) (glob: string option) = printfn $"{game} {path} {glob}"
-let list (verbose: bool) = printfn $"{verbose}"
-let info (games: string list option) = printfn $"{games}"
-let remove (games: string list) (yes: bool) = printfn $"{games} {yes}"
+type Game =
+    { Name: string
+      Path: string
+      Glob: string option }
+
+type Config =
+    { Path: string
+      Frequency: int
+      NumToKeep: int
+      Games: Game [] }
+
+let mutable config =
+    { Path = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".sbufs-backups")
+      Frequency = 15
+      NumToKeep = 20
+      Games = [||] }
+
+let backup (games: string list option) (loop: bool) (verbose: bool) =
+    printfn $"{games} {loop} {verbose}"
+    None
+
+let validGameNameChars =
+    [ [| for c in 'A' .. 'z' -> c |]
+      [| for c in '0' .. '9' -> c |]
+      [| '-'; '_' |] ]
+    |> Array.concat
+
+let isValidGameName (name: string) =
+    Array.TrueForAll(Array.ofSeq name, (fun c -> Array.contains c validGameNameChars))
+
+let add (game: string) (path: string) (glob: string option) =
+    if Array.exists (fun g -> g.Name = game) config.Games then
+        printfn "Game with the name %s already exists" game
+        None
+    else if not (isValidGameName game) then
+        printfn
+            "Invalid characters in name `%s': only alphanumeric characters, underscores, and hyphens are allowed"
+            game
+
+        None
+    else
+        let newGames =
+            Array.singleton
+                { Name = game
+                  Path = path
+                  Glob = glob }
+            |> Array.append config.Games
+            |> Array.sortBy (fun g -> g.Name)
+
+        printfn "Successfully added %s" game
+
+        Some { config with Games = newGames }
+
+let info (gameNames: string list option) (brief: bool) =
+    let games =
+        match gameNames with
+        | None -> config.Games
+        | Some gs -> Array.filter (fun g -> List.contains g.Name gs) config.Games
+
+    if brief then
+        Seq.iter (fun g -> printfn "%s" g.Name) games
+    else
+        Seq.iter
+            (fun g ->
+                printfn "Name: %s\nSave path: %s\n" g.Name g.Path
+
+                match g.Glob with
+                | None -> ()
+                | Some glob -> printfn "Save glob: %s\n\n" glob)
+            games
+
+    None
+
+let rec promptYorN prompt =
+    stdout.Flush()
+    printf "\n%s (y/n) " prompt
+
+    match Console.ReadLine().Trim().ToLower() with
+    | "y" -> true
+    | "n" -> false
+    | _ -> promptYorN prompt
+
+let remove (games: string list) (yes: bool) =
+    let newGames =
+        Array.filter
+            (fun g ->
+                if List.contains g.Name games
+                   && (yes
+                       || promptYorN ("Are you sure you want to remove " + g.Name + "?")) then
+                    printfn "Removed %s" g.Name
+                    false
+                else
+                    true)
+            config.Games
+
+    Some { config with Games = newGames }
 
 let edit (game: string) (newName: string option) (newPath: string option) (newGlob: string option) =
     printfn $"{game} {newName} {newPath} {newGlob}"
+    Some config
 
-let config (path: string option) (frequency: int option) (numToKeep: int option) =
+let editConfig (path: string option) (frequency: int option) (numToKeep: int option) =
     printfn $"{path} {frequency} {numToKeep}"
+    Some config
 
 let parser =
     ArgumentParser.Create<SbuArgs>(programName = "sbu")
+
+let defaultConfigPath =
+    Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".sbufs", "config")
 
 [<EntryPoint>]
 let main argv =
@@ -132,16 +222,39 @@ let main argv =
         let result =
             parser.ParseCommandLine(inputs = argv, raiseOnUsage = true)
 
-        match result.GetSubCommand() with
-        | Backup sp -> backup (sp.TryGetResult BackupArgs.Games) (sp.Contains Loop) (sp.Contains BackupArgs.Verbose)
-        | Add sp -> add (sp.GetResult AddArgs.Game) (sp.GetResult AddArgs.Path) (sp.TryGetResult AddArgs.Glob)
-        | List sp -> list (sp.Contains Verbose)
-        | Info sp -> info (sp.TryGetResult InfoArgs.Games)
-        | Remove sp -> remove (sp.GetResult Games) (sp.Contains Yes)
-        | Edit sp ->
-            edit (sp.GetResult Game) (sp.TryGetResult Name) (sp.TryGetResult EditArgs.Path) (sp.TryGetResult Glob)
-        | Config sp -> config (sp.TryGetResult Path) (sp.TryGetResult Frequency) (sp.TryGetResult Keep)
-        | _ -> ()
-    with e -> printfn "%s" e.Message
+        let configPath =
+            result.TryGetResult Config_Path
+            |> Option.defaultValue defaultConfigPath
+
+        config <-
+            try
+                configPath
+                |> File.ReadAllText
+                |> JsonSerializer.Deserialize<Config>
+            with e ->
+                printfn "Warning: %s Using default configuration." e.Message
+                config
+
+        let newConfig =
+            match result.GetSubCommand() with
+            | Backup sp -> backup (sp.TryGetResult BackupArgs.Games) (sp.Contains Loop) (sp.Contains Verbose)
+            | Add sp -> add (sp.GetResult AddArgs.Game) (sp.GetResult AddArgs.Path) (sp.TryGetResult AddArgs.Glob)
+            | Info sp -> info (sp.TryGetResult InfoArgs.Games) (sp.Contains Brief)
+            | Remove sp -> remove (sp.GetResult Games) (sp.Contains Yes)
+            | Edit sp ->
+                edit (sp.GetResult Game) (sp.TryGetResult Name) (sp.TryGetResult EditArgs.Path) (sp.TryGetResult Glob)
+            | Config sp -> editConfig (sp.TryGetResult Path) (sp.TryGetResult Frequency) (sp.TryGetResult Keep)
+            | Config_Path _
+            | Version -> Some config
+
+        match newConfig with
+        | None -> ()
+        | Some c ->
+            Directory.CreateDirectory(Directory.GetParent(configPath).FullName)
+            |> ignore
+
+            File.WriteAllText(configPath, JsonSerializer.Serialize(c))
+
+    with e -> printfn "Error: %s" e.Message
 
     0
